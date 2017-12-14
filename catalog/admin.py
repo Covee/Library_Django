50ede908fc6a19eb7496d769f6253eec8b2205a3
Covee <uniqtop91@gmail.com>
2017-12-14 23:47:28 +0900

    기본적인 기능 추가

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import MyModelName
+from .models import Book, Author, Genre, BookInstance
+# from .models import MyModelName
 
-class MyModelNameAdmin(admin.ModelAdmin):
-	pass
+#class MyModelNameAdmin(admin.ModelAdmin):
+	#list_display = []
+	#pass
 
-admin.site.register(MyModelName, MyModelNameAdmin)
+# admin.site.register(MyModelName, MyModelNameAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
